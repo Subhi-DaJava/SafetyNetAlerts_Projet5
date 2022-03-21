ec4868a0d19745b22a0b479df42c9e1e4daa51b7
Subhi YD <subhy809fr@gmail.com>
2022-03-22 00:05:54 +0100

    V1 Création de projet

---
+++ src/main/java/com/safetynet/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.SafetyNetAlerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/SafetyNetAlerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.SafetyNetAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
